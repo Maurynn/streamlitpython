06cf9084f8e1357df3469109eae07308c02660ef
Mauro Alves <mauro.mn@hotmail.com>
2023-06-23 01:00:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from pytube import YouTube
 from pytube.cli import on_progress
 from moviepy.editor import AudioFileClip
+import os
 
 def sanitize_filename(filename):
     bad_chars = ['\\', '/', ':', '*', '?', '"', '<', '>', '|']
@@ -9,40 +10,36 @@ def sanitize_filename(filename):
         filename = filename.replace(char, '')
     return filename
 
-def download_and_convert(url, video_quality, audio_format, convert):
+def baixar_e_converter(url, formato_audio, converter):
     yt = YouTube(url, on_progress_callback=on_progress)
-    if video_quality == "1080p":
-        video = yt.streams.get_highest_resolution()
-    else:
-        video = yt.streams.filter(progressive=True, resolution=video_quality).first()
+    video = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
     
     output_path = video.download(filename=sanitize_filename(video.title))
-    media_type = 'video/mp4'
+    tipo_midia = 'video/mp4'
     
-    if convert:
-        audio_output_path = f"{output_path.rsplit('.', 1)[0]}.{audio_format}"
+    if converter:
+        audio_output_path = f"{output_path.rsplit('.', 1)[0]}.{formato_audio}"
         AudioFileClip(output_path).write_audiofile(audio_output_path)
         output_path = audio_output_path
-        media_type = f'audio/{audio_format}'
+        tipo_midia = f'audio/{formato_audio}'
 
-    return output_path, media_type
+    return output_path, tipo_midia
 
-st.title("Baixar e Converter Vídeos do YouTube")
+st.title("YouTube Downloader & Conversor")
 
-query = st.text_input("Insira aqui a URL do vídeo do YouTube")
+url = st.text_input("Coloque aqui a URL do vídeo")
 
-video_quality = st.selectbox("Escolha a qualidade do vídeo", ["1080p", "720p", "480p", "360p", "240p"])
+formato_audio = st.selectbox("Escolha o formato do áudio", ["mp3", "wav"])
 
-audio_format = st.selectbox("Escolha o formato do áudio", ["mp3", "wav"])
-
-convert = st.checkbox("Converter para áudio?")
+converter = st.checkbox("Converter para áudio?")
 
 if st.button("Baixar"):
-    output_path, media_type = download_and_convert(query, video_quality, audio_format, convert)
-    if convert:
-        st.audio(output_path, format=media_type)
+    output_path, tipo_midia = baixar_e_converter(url, formato_audio, converter)
+    if converter:
+        st.audio(output_path, format=tipo_midia)
     else:
-        st.video(output_path, format=media_type)
+        st.video(output_path, format=tipo_midia)
+
 
 
 
